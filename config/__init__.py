f739e32ca8677060ed4da91a56b130663b024360
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-06-23 12:28:36 +0300

    feat: local env

--- config/__init__.py
+++ config/__init__.py
@@ -1,3 +1,10 @@
 import os
+import json
 
-os.environ["test"] = "test"
+LOCAL_ENV = "config/env_local.json"
+
+if os.path.exists(LOCAL_ENV):
+    with open(LOCAL_ENV) as json_file:
+        data = json.load(json_file)
+        for key, value in data.items():
+            os.environ[key] = value
