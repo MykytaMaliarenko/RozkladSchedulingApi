879f5e76e1982914df659f41a01e33edb8a0c757
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-07-06 11:50:39 +0300

    ref classes serializers: remove time slot serializer

--- djangoapps/classes/serializers.py
+++ djangoapps/classes/serializers.py
@@ -3,7 +3,6 @@ from djangoapps.classes.models import Class
 from djangoapps.groups.serializers import GroupSerializer
 from djangoapps.rooms.serializers import RoomSerializer
 from djangoapps.teachers.serializers import TeacherSerializer
-from djangoapps.timeslots.serializers import TimeSlotSerializer
 
 
 class ClassSerializer(serializers.ModelSerializer):
@@ -11,7 +10,6 @@ class ClassSerializer(serializers.ModelSerializer):
     teacher = TeacherSerializer()
     room = RoomSerializer()
     group = GroupSerializer()
-    time_slot = TimeSlotSerializer()
 
     class Meta:
         model = Class
@@ -22,7 +20,6 @@ class ClassWithoutGroupSerializer(serializers.ModelSerializer):
     """ Serializer to represent the Class model without Room """
     teacher = TeacherSerializer()
     room = RoomSerializer()
-    time_slot = TimeSlotSerializer()
 
     class Meta:
         model = Class
