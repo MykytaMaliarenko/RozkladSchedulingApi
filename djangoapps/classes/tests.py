2d8f440f511520f3bb85ad54a568d12d0b836d5f
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-07-22 14:15:04 +0300

    ref settings: add db for tests

--- djangoapps/classes/tests.py
+++ djangoapps/classes/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APILiveServerTestCase
 
-# Create your tests here.
+
+class ClassesTest(APILiveServerTestCase):
+    def test_get_classes_by_group_name(self):
+        """
+            Testing ClassesByGroupList view.
+        """
+        url = reverse('classes-by-group', kwargs={'group': 'км-92'})
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(type(response.data), type(list))
+        self.assertGreater(len(response.data), 0)
