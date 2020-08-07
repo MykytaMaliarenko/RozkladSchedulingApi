deb31887dcce491bad30216d8fe93b43a02f231b
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2020-08-07 09:57:11 +0300

    feat teachers: add TeachersSearchByNameList

--- djangoapps/teachers/urls.py
+++ djangoapps/teachers/urls.py
@@ -1,8 +1,15 @@
+from django.conf.urls import url
+from django.urls import include
 from rest_framework import routers
 
-from djangoapps.teachers.views import TeachersViewSet
+from djangoapps.teachers.views import TeachersViewSet, TeachersSearchByNameList
 
 router = routers.DefaultRouter()
 router.register(r'teachers', TeachersViewSet, basename='teachers')
 
-urlpatterns = router.urls
+urlpatterns = [
+    url(r'^teachers/search/(?P<query>.+)$',
+        TeachersSearchByNameList.as_view(), name="groups-search"),
+
+    url(r'', include(router.urls)),
+]
--- djangoapps/teachers/views.py
+++ djangoapps/teachers/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+from rest_framework import generics
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from djangoapps.teachers.models import Teacher
@@ -11,3 +13,12 @@ class TeachersViewSet(ReadOnlyModelViewSet):
 
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
+
+
+class TeachersSearchByNameList(generics.ListAPIView):
+    serializer_class = TeacherSerializer
+
+    def get_queryset(self):
+        query: str = self.kwargs["query"]
+        return Teacher.objects.filter(Q(name__icontains=query) |
+                                      Q(official_name__icontains=query))[:10]
